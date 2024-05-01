ed70037c4eb3267be08de4f3f1a2343ad36f7c58
Dipongkor-Roy <juniordipu@gmail.com>
2024-05-02 02:30:08 +0600

    basic async error solve

--- src/Pages/CheckoutForm/CheckoutForm.jsx
+++ src/Pages/CheckoutForm/CheckoutForm.jsx
@@ -26,12 +26,75 @@ const CheckoutForm = () => {
             })
         }
     },[axiosSecure,totalPrice])
-    const handleSubmit=async(event)=>{
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
-    }
+    
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return;
+        }
+    
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if (error) {
+            console.log("error", error);
+            setError(error.message);
+        } else {
+            console.log("PaymentMethod", paymentMethod);
+            setError("");
+        }
+    
+        // confirm payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous'
+                }
+            }
+        });
+        if (confirmError) {
+            console.log('confirm error');
+        } else {
+            console.log('Payment Intent :', paymentIntent);
+            if (paymentIntent.status === 'succeeded') {
+                console.log('Transaction Id :', paymentIntent.id);
+                setTransactionId(paymentIntent.id);
+    
+                // now payment save in db
+                const payment = {
+                    email: user.email,
+                    price: totalPrice,
+                    transactionId: paymentIntent.id,
+                    date: new Date(), // convert this into utc time
+                    cartIds: cart.map(item => item._id),
+                    menuIds: cart.map(item => item.menuItemId),
+                    status: 'pending'
+                };
+                const res = await axiosSecure.post('/payments', payment);
+                console.log(res.data);
+                refetch();
+                if (res.data?.paymentResult?.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Thank you",
+                        showConfirmButton: false,
+                        timer: 1000
+                    });
+    
+                    navigate('/dashboard/paymentHistory');
+                }
+            }
+        }
+    };
+    
     const card =elements.getElement(CardElement);
     if(card ===null){
         return;
